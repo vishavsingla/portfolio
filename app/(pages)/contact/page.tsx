60bf6b891f33e5a5605ab52b2f0d790d83c699d6
vishavsingla <vishav9933@gmail.com>
2024-05-21 21:41:29 +0530

    contact and resume added

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,14 +1,33 @@
-"use client"
-
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoMdMail } from 'react-icons/io';
 
-const Contact: React.FC = () => {
+const Contact = () => {
   return (
-    <div className="min-h-screen bg-[#0a192f] text-[#ccd6f6] flex flex-col items-center py-5 pt-20">
-      <h1 className="text-4xl font-bold">Contact</h1>
-      <p className="mt-4 max-w-screen-md text-gray-400">
-        C
+    <div className="min-h-screen bg-[#0a192f] text-[#ccd6f6] flex flex-col items-center p-20">
+      <h2 className="text-4xl font-bold mb-4">Get In Touch</h2>
+      <p className="text-center max-w-lg mb-8 text-gray-400">
+        I am currently looking for any new opportunities, my inbox is always open.
+        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
       </p>
+      <a href="mailto:vishavsinglaofficial@gmail.com" className="border border-[#64ffda] text-[#64ffda] py-2 px-4 rounded hover:bg-[#64ffda] hover:text-[#0a192f] transition">
+        Say Hello
+      </a>
+      <div className="flex mt-8 space-x-6">
+        <a href="https://github.com/vishavsingla" target="_blank" rel="noopener noreferrer" className="text-3xl text-[#ccd6f6] hover:text-[#64ffda] transition">
+          <FaGithub />
+        </a>
+        <a href="https://linkedin.com/in/vishav-singla" target="_blank" rel="noopener noreferrer" className="text-3xl text-[#ccd6f6] hover:text-[#64ffda] transition">
+          <FaLinkedin />
+        </a>
+        <a href="https://twitter.com/vishav_singla" target="_blank" rel="noopener noreferrer" className="text-3xl text-[#ccd6f6] hover:text-[#64ffda] transition">
+          <FaSquareXTwitter />
+        </a>
+        <a href="mailto:your-email@example.com" className="text-3xl text-[#ccd6f6] hover:text-[#64ffda] transition">
+          <IoMdMail />
+        </a>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,33 @@
 import React from 'react';
 import Link from 'next/link';
 import Projects from './(pages)/projects/page';
+import Contact from './(pages)/contact/page';
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-[#0a192f] text-[#ccd6f6] flex flex-col items-center py-5">
        <nav className="w-full flex justify-end p-4 space-x-10 pr-10">
       <Link href="/about" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">1. About</Link>
-      <Link href="/experience" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">2. Experience</Link>
-      <Link href="/projects" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">3. Projects</Link>
-      <Link href="/contact" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">4. Contact</Link>
-      <a href="/" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">5. Resume</a>
+      {/* <Link href="/experience" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">2. Experience</Link> */}
+      <Link href="/projects" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">2. Projects</Link>
+      <Link href="/contact" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">3. Contact</Link>
+      <a href="/Resume.pdf" className="text-[#64ffda] text-sm hover:border-b-2 hover:pb-2 hover:border-[#64ffda] transition duration-300">4. Resume</a>
     </nav>
       <div className="mt-16 max-w-screen-lg">
         <h1 className="text-[#64ffda] text-2xl">Hi, my name is</h1>
         <h2 className="text-7xl font-bold mt-4">Vishav Singla.</h2>
-        <h3 className="text-4xl text-[#25647F] font-bold mt-2 text-gray-300">I am a third year undergraduate at Thapar University.</h3>
+        <h3 className="text-4xl text-[#25647F] font-bold mt-2 text-gray-300">I am a third year undergraduate at Thapar University, India.</h3>
         <p className="mt-4 text-gray-400">
           I’m a software engineer based in India specializing in building Full Stack applications and Blockchain based applications. 
           I have experience working with a range of technologies including React.js, Next.js, Node.js, Express.js, Solidity, Typescript, SQL, PostgreSQL, Docker and Ethereum.
         </p>
         <a href="https://github.com/vishavsingla" className="inline-block mt-8 border border-[#64ffda] text-[#64ffda] py-2 px-4 rounded text-sm">Check out my Github!</a>
       </div>
-      <div className='m-14'>
+      <div className='mt-14 mx-14'>
         <Projects />
+      </div>
+      <div className=''>
+        <Contact />
       </div>
       
     </div>
